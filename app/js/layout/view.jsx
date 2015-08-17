c76bb0a43389f8a6dde8a165b186400105f8f036
Tyler Benton <tjbenton21@gmail.com>
2015-08-17 08:18:02 -0400

    changed `class` to `className` to fix an error with react.

--- app/js/layout/view.jsx
+++ app/js/layout/view.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-component";
-
 let View = React.createClass({
  render(){
   var links = [1, 2, 3, 4, 5].map((item) =>
    <li key={item}><Link href={`/${item}`}>Item {item}</Link></li>
   );
   return (
-   <div id="s-content" class="s-content">
+   <div id="s-content" className="s-content">
     <ul>{
      links
     }</ul>
