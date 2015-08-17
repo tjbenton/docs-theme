e1143d15612cc56e96c8970413b9a983c427c8b0
Tyler Benton <tjbenton21@gmail.com>
2015-08-17 08:20:25 -0400

    Renamed `ContactStore` to `DocsStore`

---
+++ app/js/stores/DocsStore.js
@@ -0,0 +1,48 @@
+let _init_called = false,
+    _change_listeners = [],
+    _json = {},
+    DocsStore = module.exports = {
+     init(){
+      if(_init_called){
+       return;
+      }
+      return ajax("./docs.json")
+      .then((response) => {
+       _json = response;
+       DocsStore.notifyChange();
+      })
+      .catch((err) => {
+       console.error(err);
+      });
+     },
+     notifyChange(){
+      _change_listeners.forEach(function(listener){
+       listener();
+      });
+     },
+     addChangeListener(listener){
+      _change_listeners.push(listener);
+     },
+
+     removeChangeListener(listener){
+      _change_listeners = _change_listeners.filter(function(l){
+       return listener !== l;
+      });
+     },
+     getDocs(){
+      return _json;
+     }
+    };
+
+localStorage.token = localStorage.token || (Date.now() * Math.random());
+
+function ajax(url){
+ return new Promise((resolve, reject) => {
+  let req = new XMLHttpRequest;
+  req.open("GET", url);
+  req.onload = () => resolve(req.responseText);
+  req.onerror = reject;
+  req.setRequestHeader("authorization", localStorage.token);
+  req.send();
+ });
+}
